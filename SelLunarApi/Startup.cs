18ba62b26d2b21e5386f59dad060c4a8823d078f
yong <kyt8724@gmail.com>
2015-11-09 09:16:07 -0500

    Home/Index.cshtml & Home/About.cshtml by YTKim on 9 Nov 2015

---
+++ SelLunarApi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SelLunarApi.Startup))]
+namespace SelLunarApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
